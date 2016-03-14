63f64e261634354104260d12b061cc0d56d10195
dominiccarr <dominiccarr@gmail.com>
2016-03-14 14:28:23 +0000

    first commit, adding all devleoped files

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Item.create(title: 'Mizuno MP 630 Fast Track Driver', description: 'Great Clib', price: 299.0, category: 'Drivers', image_url: 'mizuno.jpg')
+
+Item.create(title: 'Titleist 712 MB Irons', description: 'Great Irons', price: 899, category: 'Irons', image_url: 'titleist.jpg')
+
+Item.create(title: 'Vokey Wedge', description: 'Great Wedge', price: 125, category: 'Irons', image_url: 'wedge.jpg')
